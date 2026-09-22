a1ac36c58d5726013270313fd035fd794c50c66e
albert-sobreo <janalbertsobreo@gmail.com>
2026-09-22 10:30:31 +0800

    Initial Commit

---
+++ Agdayo/AgdayoApp.swift
@@ -0,0 +1,32 @@
+//
+//  AgdayoApp.swift
+//  Agdayo
+//
+//  Created by Jan Albert Sobreo on 9/22/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AgdayoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
